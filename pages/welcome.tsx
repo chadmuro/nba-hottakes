0eb9819338d0cbcd4e6a2d3545a824920df803e1
chadmuro <chadmuro@gmail.com>
2023-04-23 14:01:53 +0900

    feat: Install lottie-react and update welcome page UI

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
 import { useEffect } from "react";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import Lottie from "lottie-react";
 import Layout from "../components/Layout";
+import flamesAnimation from "../public/lottie/flames.json";
 import useRedirectAfterSomeSeconds from "../hooks/useRedirectAfterSomeSeconds";
 
 export default function Welcome() {
@@ -17,7 +20,6 @@ export default function Welcome() {
         return;
       }
     }
-
     if (supabase && session) {
       createUser(session.user.id);
     }
@@ -25,9 +27,19 @@ export default function Welcome() {
 
   return (
     <Layout>
-      <div className="w-full prose flex flex-col items-center">
-        <h1>Thank you for joining NBA Hot Takes!</h1>
+      <div className="w-full prose flex flex-col items-center relative">
+        <h1 className="mt-20 text-center">
+          Thank you for joining NBA Hot Takes!
+        </h1>
         <p>Redirecting in: {secondsRemaining}</p>
+        <Link href="/profile" className="btn">
+          Go to profile page
+        </Link>
+        <Lottie
+          animationData={flamesAnimation}
+          loop={true}
+          className="w-full absolute bottom-0 -z-10"
+        />
       </div>
     </Layout>
   );
