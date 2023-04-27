cb7f3805b94408b2feee8deac945cf10b59418a2
chadmuro <chadmuro@gmail.com>
2023-04-27 22:56:15 +0900

    feat: Add 404 and 500 error pages

---
+++ pages/500.tsx
@@ -0,0 +1,18 @@
+import Lottie from "lottie-react";
+import Link from "next/link";
+import Layout from "../components/Layout";
+import trashAnimation from "../public/lottie/trash.json";
+
+export default function Custom500() {
+  return (
+    <Layout>
+      <div className="w-full prose flex flex-col items-center relative">
+        <h1 className="mt-20 text-center">500 - Server-side error occurred</h1>
+        <Link href="/" className="btn mb-4">
+          Back to Home
+        </Link>
+        <Lottie animationData={trashAnimation} loop={true} className="w-3/4" />
+      </div>
+    </Layout>
+  );
+}
